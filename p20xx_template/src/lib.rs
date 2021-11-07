c50835b3ba1ef135e988fc294d27b7816b928f5d
Jocelyn Stericker <jocelyn@nettek.ca>
2021-11-07 10:06:54 -0500

    Updates to template

--- p20xx_template/src/lib.rs
+++ p20xx_template/src/lib.rs
@@ -1,5 +1,3 @@
-// use std::collections::HashSet;
-
 pub fn part_a(input: &str) -> i64 {
     for line in input.trim().split('\n') {
         //
@@ -9,13 +7,13 @@ pub fn part_a(input: &str) -> i64 {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn example1() {
-        assert_eq!(super::part_a("\n"), 0);
-    }
-
     // #[test]
-    // fn part_a() {
-    //     assert_eq!(super::part_a(include_str!("input.txt")), 0);
+    // fn example1() {
+    //     assert_eq!(super::part_a("\n"), 0);
     // }
+
+    #[test]
+    fn part_a() {
+        assert_eq!(super::part_a(include_str!("input.txt")), 0);
+    }
 }
